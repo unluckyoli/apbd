69d28a2b2844d5227954e5d3626f5bd203e00a31
oli <s28526@pjwstk.edu.pl>
2024-03-10 00:19:13 +0100

    dodano pole position

--- Program.java
+++ Program.java
@@ -1,5 +1,7 @@
 public class Program {
     public static void main(String[] args) {
-        System.out.println("LANDO NORRIS LEADS BRITISH GRAND PRIX");
+        System.out.println("LANDO NORRIS LEADS THE BRITISH GRAND PRIX");
+
+        System.out.println("AND LANDO NORRIS IS ON POLE");
     }
 }
